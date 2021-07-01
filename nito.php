3ebeca0d3dae8b2411e8ae34a51d632ee5f00ea9
030236200019 <84632372+030236200019@users.noreply.github.com>
2021-07-01 13:30:15 +0700

    Add files via upload

---
+++ nito.php
@@ -0,0 +1,375 @@
+<?php
+error_reporting(0);
+$res="\033[0m"; 
+$BBlack="\033[1;30m" ; 
+$BRed="\033[1;31m" ; 
+$BGreen="\033[1;32m" ; 
+$BYellow="\033[1;33m" ; 
+$BBlue="\033[1;34m" ; 
+$BPurple="\033[1;35m" ; 
+$BCyan="\033[1;36m" ; 
+$BWhite="\033[1;37m" ; 
+$Blue="\033[0;34m"; 
+$lime="\033[1;32m"; 
+$red="\033[1;31m"; 
+$xanh="\033[1;32m"; 
+$cyan="\033[1;36m"; 
+$yellow="\033[1;33m"; 
+$turquoise="\033[1;34m"; 
+$maugi="\033[1;35m"; 
+$white= "\033[1;37m"; 
+$red="\033[1;31m"; 
+$white= "\033[1;37m"; 
+$whiteb= "\033[1;37m"; 
+$redb="\033[1;31m"; 
+$green="\033[1;32m"; 
+$yellow="\033[1;33m"; 
+$cam="\033[1;33m"; 
+$test="\033[1;33m"; 
+$greenb="\033[1;32m"; 
+$blue="\033[1;34m"; 
+$lam="\033[1;34m"; 
+$tmi="\033[1;34m"; 
+$hong="\033[1;35m"; 
+$imt="\033[1;35m"; 
+$cyan= "\e[1;36m"; 
+$syan="\033[1;36m"; 
+$xnhac= "\033[1;96m"; 
+$den="\033[1;90m"; 
+$do="\033[1;91m"; 
+$luc="\033[1;92m"; 
+$vang="\033[1;93m"; 
+$xduong="\033[1;94m"; 
+$hong="\033[1;95m"; 
+$trang="\033[1;97m"; 
+$vang="\033[1;93m"; 
+$do="\033[1;91m"; 
+$BBlack="\033[1;30m"; 
+$BRed="\033[1;31m"; 
+$BGreen="\033[1;32m"; 
+$BYellow="\033[1;33m"; 
+$BBlue="\033[1;34m"; 
+$BPurple="\033[1;35m"; 
+$BCyan="\033[1;36m"; 
+$BWhite="\033[1;37m"; 
+$Blue="\033[0;34m"; 
+$lime="\033[1;32m"; 
+$red="\033[1;31m"; 
+$xanh="\033[1;32m"; 
+$cyan="\033[1;36m"; 
+$yellow="\033[1;33m"; 
+$turquoise="\033[1;34m"; 
+$maugi="\033[1;35m"; 
+$white= "\033[1;37m"; 
+$BCyan="\033[1;36m"; 
+$trang="\033[1;37m"; 
+$do="\033[1;31m"; 
+$luc="\033[1;32m"; 
+$vang="\033[1;33m"; 
+$luc="\033[1;92m"; 
+
+@system('clear'); $useragent="Mozilla/5.0 
+(Linux; Android 10; SM-J600G) 
+AppleWebKit/537.36 (KHTML, like Gecko) 
+Chrome/83.0.4103.106 Mobile 
+Safari/537.36"; 
+$traodoisub="http://run.nitotool.com/v3/traodoisub.json"; 
+$tuongtaccheo="http://run.nitotool.com/v3/tuongtaccheo.json"; 
+$qilizz="http://run.nitotool.com/v3/qilizz.json";//9 
+$nuoifb="http://run.nitotool.com/v3/nuoifb.json";//10 
+$spamcmt="http://run.nitotool.com/v3/spamcmt.json";//11 
+$spamshare="http://run.nitotool.com/v3/spamshare.json";//12 
+$addbb="http://run.nitotool.com/v3/addbb.json";//14 
+$chocbb="http://run.nitotool.com/v3/chocbb.json";//15 
+$get2fa="http://run.nitotool.com/v3/get2fa.json";//16 
+$gettoken="http://run.nitotool.com/v3/gettoken.json";//13 
+$kiemtra="http://run.nitotool.com/v3/kiemtra.json";//14 
+$l2l="http://run.nitotool.com/v3/l2l.json";//19 
+$tlf="http://run.nitotool.com/v3/tlf.json";//20 
+$teamlike="http://run.nitotool.com/v3/teamlike.json";//21 
+$adsvideo="http://run.nitotool.com/v3/adsvideo.json";//22 
+$adsvlog="http://run.nitotool.com/v3/adsvlog.json";//23 
+$locmai="http://run.nitotool.com/v3/locmai.json";//24 
+$micha="http://run.nitotool.com/v3/micha.json";//25 
+$lock="http://run.nitotool.com/v3/lock.json";//26 
+$gomlua="http://run.nitotool.com/v3/gomlua.json";//27 
+$lock="http://run.nitotool.com/v3/lock.json"; 
+$gomluadl="http://run.nitotool.com/v3/gomluadl.json"; 
+$bom="http://run.nitotool.com/v3/bom.json"; 
+$link1s="http://run.nitotool.com/v3/link1s.json"; 
+$mega="http://run.nitotool.com/v3/mega.json"; 
+$fly="http://run.nitotool.com/v3/fly.json"; 
+$ouo="http://run.nitotool.com/v3/ouo.json"; 
+$cmt1id="http://run.nitotool.com/v3/cmt1id.json"; 
+$vig="http://run.nitotool.com/v3/vig.json"; 
+$vigdl="http://run.nitotool.com/v3/vigdl.json"; 
+$rk="http://run.nitotool.com/v3/rk.json"; 
+$huyfl1="http://run.nitotool.com/v3/huyfl1.json"; 
+$monkey="http://run.nitotool.com/v3/monkey.json"; 
+$cmta="http://run.nitotool.com/v3/cmta.json"; 
+$nuoidl="http://run.nitotool.com/v3/nuoidl.json"; 
+$tree="http://run.nitotool.com/v3/tree.json"; 
+$tdcck="http://run.nitotool.com/v3/tdcck.json"; 
+$tdctk="http://run.nitotool.com/v3/tdctk.json"; 
+
+echo $xduong."\n";
+for ($makep=38;$makep > 0;$makep--){echo $res."-";usleep(2500);echo $red."-";}
+
+echo "\n";echo" 
+   \033[1;91m███╗   ██╗██╗████████╗ ██████╗    \033[0;33m  ████████╗ ██████╗  ██████╗ ██╗
+   \033[0;33m████╗  ██║██║╚══██╔══╝██╔═══██╗    \033[0;31m ╚══██╔══╝██╔═══██╗██╔═══██╗██║
+   \033[1;94m██╔██╗ ██║██║   ██║   ██║   ██║ ███\033[1;95m██╗ ██║   ██║   ██║██║   ██║██║
+   \033[1;92m██║╚██╗██║██║   ██║   ██║   ██║╚ ══\033[1;96m══╝ ██║   ██║   ██║██║   ██║██║
+   \033[1;95m██║ ╚████║██║   ██║   ╚██████╔╝   \033[1;94m     ██║   ╚██████╔╝╚██████╔╝███████╗
+   \033[1;96m╚═╝  ╚═══╝╚═╝   ╚═╝    ╚═════╝   \033[0;32m      ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝  \n\n";
+ for ($makep=38;$makep > 0;$makep--){echo $res."-";usleep(2500);echo $red."-";}
+echo "\n";
+echo $maufulldo." NiTo TOOL Share Tool TDS Và Nhiều Tool Khác  $res";
+echo "\n\n";
+
+echo "\033[1;37m┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[1;36m━┓\n";
+
+usleep(50000);
+
+echo "\033[1;97m┃ ~\033[1;91m[\033[1;102;90m 1 \033[0;1;91m] \033[1;96m => Tool Trao Đổi Sub Cookie Đơn      ┃\n";
+
+usleep(50000);
+
+echo "\033[1;97m┃ ~\033[1;91m[\033[1;106;90m 2 \033[0;1;91m] \033[1;92m => Tool Trao Đổi Sub Cookie Đa Luồng ┃\n";
+
+usleep(50000);
+
+echo "\033[1;97m┃ ~\033[1;91m[\033[1;105;90m 3 \033[0;1;91m] \033[1;93m => Tool Trao Đổi Sub Token Đơn       ┃\n";
+
+usleep(50000);
+
+echo "\033[1;97m┃ ~\033[1;91m[\033[1;105;90m 4 \033[0;1;91m] \033[1;93m => Tool Trao Đổi Sub Token Đa Luồng  ┃\n";
+
+usleep(50000);
+
+echo "\033[1;97m┃ ~\033[1;91m[\033[1;103;90m 5 \033[0;1;91m] \033[1;91m => Tool TuongTacCheo Cookie Đơn      ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;91m[\033[1;101;90m 6 \033[0;1;91m] \033[1;34m => Tool TuongTacCheo Cookie Đa Luồng ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;91m[\033[1;106;90m 7 \033[0;1;91m] \033[1;35m => Tool TuongTacCheo Token Đơn       ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;91m[\033[1;102;90m 8 \033[0;1;91m]\033[1;33m  => Tool TuongTacCheo Token Đa Luồng  ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;91m[\033[1;107;90m 9 \033[0;1;91m] \033[1;32m => Tool VIPIG COKIE Đơn              ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;104;90m 10 \033[0;1;31m] \033[1;31m => Tool VIPIG COKIE Đa Luồng        ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;105;90m 11 \033[0;1;31m] \033[1;35m=> Tool Traodoicheo Cookie           ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;106;90m 12 \033[0;1;31m] \033[1;32m=> Tool Traodoicheo Token            ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;103;90m 13 \033[0;1;31m] \033[1;97m=> Tool Qlizz                        ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;102;90m 14 \033[0;1;31m] \033[1;31m=>Tool Nuôi Facebook                 ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;101;90m 15 \033[0;1;31m]\033[1;35m => Tool Nuôi Facebook Đa Luồng       ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;107;90m 16 \033[0;1;31m] \033[1;37m=> Tool Spam Cmt Đa Luồng            ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;105;90m 17 \033[0;1;31m] \033[1;36m=> Tool Spam Cmt 1 ID                ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;106;90m 18 \033[0;1;31m] \033[1;34m=> Tool Spam Cmt Ảnh Group           ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;103;90m 19 \033[0;1;31m] \033[1;32m=> Tool Spam Share                   ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;102;90m 20 \033[0;1;31m] \033[1;33m=> Tool Auto Hủy Follow              ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;104;90m 21 \033[0;1;31m] \033[1;31m=> Tool Gửi Lời Mời Kết Bạn          ┃\n";
+
+usleep(50000);
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;103;90m 22 \033[0;1;31m] \033[1;32m=> Tool Chọc Full Bạn Bè             ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;102;90m 23 \033[0;1;31m] \033[1;33m=> Tool Get Mã 2FA                   ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;104;90m 24 \033[0;1;31m] \033[1;31m=> Tool Get Token Full Quyền         ┃\n";
+
+usleep(50000);
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;103;90m 25 \033[0;1;31m] \033[1;32m=> Tool Check Full Info Facebook     ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;102;90m 26 \033[0;1;31m] \033[1;33m=> Tool Bật Lock Profile             ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;104;90m 27 \033[0;1;31m] \033[1;31m=> Tool Like 2 Like Đa Luồng         ┃\n";
+
+usleep(50000);
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;103;90m 28 \033[0;1;31m] \033[1;32m=> Tool Tăng Like Free               ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;102;90m 29 \033[0;1;31m] \033[1;33m=> Tool Gom Lúa                      ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;104;90m 30 \033[0;1;31m] \033[1;31m=> Tool Gom Lúa Đa Luồng             ┃\n";
+
+usleep(50000);
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;103;90m 31 \033[0;1;31m] \033[1;32m=> Tool Team Like                    ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;102;90m 32 \033[0;1;31m] \033[1;33m=> Tool Rút Gọn Link Kiếm Tiền       ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;104;90m 33 \033[0;1;31m] \033[1;31m=> Tool Rankmedia.app                ┃\n";
+
+usleep(50000);
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;103;90m 34 \033[0;1;31m] \033[1;32m=> Tool Monkey Rewards               ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;102;90m 35 \033[0;1;31m] \033[1;33m=> Tool Cashtree                     ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;104;90m 36 \033[0;1;31m] \033[1;31m=> Tool Adsvlogs.online              ┃\n";
+
+usleep(50000);
+
+
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;107;90m 37 \033[0;1;31m] \033[1;35m=> Tool Get + Lọc Mail               ┃\n";
+
+usleep(50000);
+echo "\033[1;37m┃ ~\033[1;31m[\033[1;107;90m 38 \033[0;1;31m] \033[1;35m=> Exit Tool                         ┃\n";
+
+usleep(50000);
+
+echo "\033[1;37m┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n";
+
+usleep(1000);
+echo "\e[1;34m┌─[\e[1;37m\e[1;42mVui Lòng Nhập Tools !! \e[0m\e[1;34m]
+└──╼ \e[1;35mĐ\e[1;36mẠ\e[1;31mT $yellowb ";
+
+$chedo = trim(fgets(STDIN)); if ($chedo>0 
+&& $chedo<5){ //trao đổi sub 
+eval(file_get_contents($traodoisub)); } else 
+if ($chedo >4 && $chedo < 9){ //tươngtácchéo 
+eval(file_get_contents($tuongtaccheo)); } 
+elseif ($chedo == "9") { //vipig.net đơn 
+eval(file_get_contents($vig)); } elseif 
+($chedo == "10") { //vipig.net đa luồng 
+eval(file_get_contents($vigdl)); } elseif 
+($chedo == "11") { //trao đổi chéo cookie 
+eval(file_get_contents($tdcck)); } elseif 
+($chedo == "12") { //trao đổi chéo token 
+eval(file_get_contents($tdctk)); } elseif 
+($chedo == "13") { //ᴛᴏᴏʟ qilizz 
+eval(file_get_contents($qilizz)); } elseif 
+($chedo == "14") { //nuôi fb 
+eval(file_get_contents($nuoifb)); } elseif 
+($chedo == "15") { //nuôi fb đa luồng 
+eval(file_get_contents($nuoidl)); } elseif 
+($chedo == "16") { //Spam Cmt cookie 
+eval(file_get_contents($spamcmt)); } else if 
+($chedo == "17") { //spam cmt 1 id 
+eval(file_get_contents($cmt1id)); } else if 
+($chedo == "18") { //spam cmt ảnh 
+eval(file_get_contents($cmta)); } else if
+($chedo == "19") { //spam share 
+eval(file_get_contents($spamshare)); } else if 
+($chedo == "20") { //hủy follow 
+eval(file_get_contents($huyfl1)); } else if 
+($chedo == "21") { //thêm bạn bè 
+eval(file_get_contents($addbb)); } else if 
+($chedo == "22") { //chọc bạn bè 
+eval(file_get_contents($chocbb)); } else if 
+($chedo == "23") { //get mã 2fa 
+eval(file_get_contents($get2fa)); } elseif 
+($chedo == "24") { //gét token fb 
+eval(file_get_contents($gettoken)); } elseif 
+($chedo == "25") { //xem thông tin Facebook 
+eval(file_get_contents($kiemtra)); } elseif 
+($chedo == "26") { //locked profile 
+eval(file_get_contents($lock)); } elseif 
+($chedo == "27") { //like2like đa luồng 
+eval(file_get_contents($l2l)); } elseif 
+($chedo == "28") { //Tăng Like free 
+eval(file_get_contents($tlf)); } elseif 
+($chedo == "29") { //gom lúa đơn 
+eval(file_get_contents($gomlua)); } elseif 
+($chedo == "30") { //gom lúa 
+eval(file_get_contents($gomluadl)); } elseif 
+($chedo == "31") { //team like 
+eval(file_get_contents($teamlike)); } elseif 
+($chedo == "32") { //rút gọn link 
+
+ for($i=0; 
+$i<=13; $i++){ echo "\033[1;31m─ "; 
+usleep(20000); echo "\033[1;37m─ "; 
+usleep(20000); } echo "\n"; 
+echo "\033[1;32m ɴʜậᴘ" .$do." [".$vang."1".$do."] " .$luc."ʙᴏᴍ.ᴛᴏ\n"; 
+echo "\033[1;32m ɴʜậᴘ" .$do." [".$vang."2".$do."] " .$luc."ʟɪɴᴋ1s.ᴄᴏᴍ\n"; 
+echo "\033[1;32m ɴʜậᴘ" .$do." [".$vang."3".$do."] " .$luc."ᴍᴇɢᴀᴜʀʟ.ɪɴ\n"; 
+echo "\033[1;32m ɴʜậᴘ" .$do." [".$vang."4".$do."] " .$luc."sʜʀᴛғʟʏ.ᴄᴏᴍ\n"; 
+echo "\033[1;32m ɴʜậᴘ" .$do." [".$vang."5".$do."] " .$luc."ᴏᴜᴏ.ɪᴏ\n"; for($i=0; $i<=13; $i++){ echo 
+"\033[1;31m─ "; usleep(20000); echo 
+"\033[1;37m─ "; usleep(20000); } echo "\n"; 
+echo "\033[1;37m~\033[1;31m[\033[1;33m●\033[1;31m]\033[1;37m 
+=>\033[1;36m ɴʜậᴘ ᴄʜế Độ: \033[1;33m"; 
+$nhap=trim(fgets(STDIN)); if ($nhap == "1") 
+{ eval(file_get_contents($bom)); } else if 
+($nhap == "2") { 
+eval(file_get_contents($link1s)); } else if 
+($nhap == "3") { 
+eval(file_get_contents($mega)); } else if 
+($nhap == "4") { 
+eval(file_get_contents($fly)); } else if ($nhap 
+== "5") { eval(file_get_contents($ouo)); } else 
+{exit("\033[1;37m~\033[1;31m[✖] ʟựᴀ ᴄʜọɴ 
+ᴋʜôɴɢ xáᴄ Địɴʜ\n");} } else if ($chedo == 
+"33") { //rankmedia.app 
+eval(file_get_contents($rk)); } else if ($chedo 
+== "34") { //monkey 
+eval(file_get_contents($monkey)); } else if 
+($chedo == "35") { //cashtree 
+eval(file_get_contents($tree)); } else if 
+($chedo == "36") { //adsvlog.com 
+eval(file_get_contents($adsvlog)); } elseif 
+($chedo == "37") { //lọc mail 
+eval(file_get_contents($locmai)); } 
+
+?>
